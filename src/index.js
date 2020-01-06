cb2610d568ceeffcd181c055de60790243d50cb3
mlepe <m.leperlier42@gmail.com>
2020-01-06 15:58:22 +0100

    - Légère avancée : sprite joueur qui se déplace

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import Entity from "./modules/entity.js";
-
 var root = this;
 
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    background: 'black',
+    physics:  {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 0 },
+            debug: false
+        }
+    },
     scene: {
         mode: Phaser.Scale.NONE,
         parent: 'shootemup',
@@ -19,12 +25,37 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
+    this.load.image('playerShip', 'assets/player_ship.png');
 }
 
 function create ()
 {
+    //this.add.image(0, 0, 'playerShip');
+
+    player = this.physics.add.sprite(100, 450, 'playerShip');
+
+    player.setCollideWorldBounds(true);
+
+    cursors = this.input.keyboard.createCursorKeys();
 }
 
 function update ()
 {
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+    }
+    else
+    {
+        player.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
 }
